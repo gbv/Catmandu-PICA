1a240161e0e24e6ee0b609cb6f31c5e9f8299507
Jakob Voss <voss@gbv.de>
2021-08-17 09:27:35 +0200

    Fix broken SRU importer with PICA::Data >= 1.27
    See <https://github.com/gbv/PICA-Data/issues/107#issuecomment-899680802>

--- lib/Catmandu/Importer/SRU/Parser/picaxml.pm
+++ lib/Catmandu/Importer/SRU/Parser/picaxml.pm
@@ -8,10 +8,11 @@ use PICA::Parser::XML;
 sub parse {
     my ( $self, $record ) = @_;
 
-    my $xml = $record->{recordData}->toString();
-    my $parser = PICA::Parser::XML->new( $xml ); 
+    my $xml    = $record->{recordData}->toString();
+    my $parser = PICA::Parser::XML->new($xml);
 
-    return $parser->next;
+    my $next = $parser->next;
+    return $next ? {%$next} : undef;
 }
 
 1;
