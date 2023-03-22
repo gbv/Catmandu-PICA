b254cfb92ac1f76c6dcfafcb930952bcb0a875f2
Jakob Voss <voss@gbv.de>
2023-03-22 22:16:12 +0100

    Start pica_diff binder (#81)

---
+++ lib/Catmandu/Fix/Bind/pica_diff.pm
@@ -0,0 +1,52 @@
+package Catmandu::Fix::Bind::pica_diff;
+
+our $VERSION = '1.10';
+
+use Moo;
+use Catmandu::Sane;
+use PICA::Data qw(pica_fields pica_annotation pica_title pica_diff);
+use Scalar::Util 'reftype';
+
+with 'Catmandu::Fix::Bind', 'Catmandu::Fix::Bind::Group';
+
+sub bind {
+    my ( $self, $data, $code ) = @_;
+    return unless reftype( $data->{record} ) eq 'ARRAY';
+
+    my $ppn = pica_fields( $data->{record}, '003@' );
+
+    # TODO: pica_fields should do a copy
+    my $before = [ map { [@$_] } @{ $data->{record} } ];
+
+    $code->($data);
+    $data->{record} = pica_diff( $before, $data->{record} );
+
+    # Add record identifier
+    if ( @{ $data->{record}{record} } && @$ppn ) {
+        pica_annotation( $ppn->[0], ' ' );
+        unshift @{ $data->{record}{record} }, @$ppn;
+    }
+
+    return $data;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Catmandu::Fix::Bind::pica_diff - a binder that tracks changes in PICA records
+
+=head1 SYNOPSIS
+
+  do pica_diff()
+    pica_set(foo,021A$a)
+    pica_add(foo,010@$x)
+  end
+ 
+=head1 DESCRIPTION
+
+This binder replaces a record with a L<PICA Patch|https://format.gbv.de/pica/patch>
+record of changes applied to the original record.
+
+=cut
---
+++ t/20-pica-diff.t
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use Test::More;
+
+use Catmandu::Importer::PICA;
+use Catmandu::Fix;
+use PICA::Data 'pica_string';
+
+my $fixer = Catmandu::Fix->new( fixes => [q|
+set_field(foo,"bar")
+do pica_diff()
+    pica_set(foo,021A$a)
+    pica_add(foo,010@$x)
+end
+|]);
+my $importer = Catmandu::Importer::PICA->new( file => "./t/files/minimal.pp", type => "Plain" );
+my $record = $fixer->fix( $importer->first );
+is pica_string($record->{record}), <<'PICA', "pica_diff";
+  003@ $0123
++ 010@ $xbar
+- 021A $aabc
++ 021A $abar
+
+PICA
+
+done_testing;
