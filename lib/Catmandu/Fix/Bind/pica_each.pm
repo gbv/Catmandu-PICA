81bd7cc29522c291d88c80fc7680fb5a12eae3dc
Jakob Voss <voss@gbv.de>
2023-03-27 12:04:38 +0200

    Adjust syntax of PICA Path in examples (#75)

--- lib/Catmandu/Fix/Bind/pica_each.pm
+++ lib/Catmandu/Fix/Bind/pica_each.pm
@@ -65,8 +65,8 @@ Catmandu::Fix::Bind::pica_each - a binder that loops over PICA fields
     # Only add field 039D subfield $9 to the editions when the subfield $a 
     contains a 'E-Paper'
     do pica_each()
-        if pica_match("039Da","E-Paper")
-            pica_map("039D9",editions.$append)
+        if pica_match("039D$a","E-Paper")
+            pica_map("039D$9",editions.$append)
         end
     end
 
