fb1fd8a31d70d9bf56071c817919abe14093a9ab
Jakob Voss <voss@gbv.de>
2023-08-18 08:10:02 +0200

    Checking in changes prior to tagging of version 1.17.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6e3b16f..d115031 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Changelog for Catmandu-PICA
     
     {{$NEXT}}
    +
    +1.17 2023-08-18T06:06:33Z
       - Add Fix: pica_append (#82)
     
     1.16 2023-08-11T07:06:23Z

--- lib/Catmandu/Fix/Condition/pica_match.pm
+++ lib/Catmandu/Fix/Condition/pica_match.pm
@@ -1,6 +1,6 @@
 package Catmandu::Fix::Condition::pica_match;
 
-our $VERSION = '1.16';
+our $VERSION = '1.17';
 
 use Catmandu::Sane;
 use Catmandu::Fix::pica_map;
--- lib/Catmandu/PICA.pm
+++ lib/Catmandu/PICA.pm
@@ -1,6 +1,6 @@
 package Catmandu::PICA;
 
-our $VERSION = '1.16';
+our $VERSION = '1.17';
 
 1;
 __END__
